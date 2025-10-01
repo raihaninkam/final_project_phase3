8525bdcdd457db08a33e2489f5b8494276a2cd5c
Raihaninkam <raihankamil37@gmail.com>
2025-10-01 16:03:50 +0700

    feat(model): add model for post response

--- internals/models/post.model.go
+++ internals/models/post.model.go
@@ -27,3 +27,18 @@ type PostWithUser struct {
 	UserName   *string   `json:"user_name"`
 	UserAvatar *string   `json:"user_avatar"`
 }
+
+type Posting struct {
+	ID            string    `json:"id" db:"id"`
+	UserID        string    `json:"user_id" db:"user_id"`
+	Content       string    `json:"content" db:"content"`
+	ImageUrl      string    `json:"image_url,omitempty" db:"image_url"`
+	CreatedAt     time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at" db:"updated_at"`
+	UserName      string    `json:"user_name,omitempty" db:"user_name"`
+	UserAvatarUrl *string   `json:"user_avatar_url,omitempty" db:"user_avatar_url"`
+	LikeCount     int       `json:"like_count" db:"like_count"`
+	CommentCount  int       `json:"comment_count" db:"comment_count"`
+	FollowerCount int       `json:"follower_count" db:"follower_count"`
+	IsLiked       bool      `json:"is_liked" db:"is_liked"`
+}
